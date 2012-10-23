b00d54ed61e2a559def9c2adad64ce83d23c6388
YefimovAV <shuriks1990@gmail.com>
2012-10-23 15:52:53 +0400

    initial commit

---
+++ ANW_Console/ANW_Console/ANW_Console.cpp
@@ -0,0 +1,20 @@
+// ANW_Console.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "agg_win32_bmp.h"
+#include <boost/filesystem.hpp>
+
+using namespace std;
+
+void ReadFolder() {
+
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello world!!!" << endl; 
+	return 0;
+}
+
