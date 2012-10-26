e3a26dd58de9dd8cf54b3d5839d99826fd38cad2
Yefimov <shuriks1990@gmail.com>
2012-10-26 15:29:12 +0400

    Read and write names and path of teaching files.

--- ANW_Console/ANW_Console/ANW_Console.cpp
+++ ANW_Console/ANW_Console/ANW_Console.cpp
@@ -8,20 +8,40 @@
 
 using namespace std;
 
-void ReadFolder() {
-
+void ReadFolder(string path) {
+	namespace fs = boost::filesystem;
+	ofstream pathList;
+	pathList.open("result/pathListTeach.txt");
+	cout << "Using files:" << endl; 
+	for (fs::directory_iterator it(path), end; it != end; ++it) {
+        if (it->path().extension() == ".bmp") {
+			cout << it->path().filename() << endl;
+			pathList << *it << endl;
+		}
+	}
+	pathList.close();
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	string mode;
+	char answer = 'y';
 	for(;;) {
-	cout << "Choose a mode (teach/recognition): ";
-	cin >> mode;
-	if(mode == "teach") { cout << "You choose Teach mode" << endl; break; }
-		//Teach(), break; 
-	else if(mode == "recognition") { cout << "You choose recognition mode" << endl; break; }
-		//Recognition(), break;
+	if(answer == 'y') {
+		cout << "Choose a mode (teach/recognition): ";
+		cin >> mode;
+		if(mode == "teach") { 
+			cout << "You choose Teach mode" << endl; 
+			ReadFolder("teach/");  
+		}
+		else if(mode == "recognition") { cout << "You choose recognition mode" << endl; }
+			//Recognition(), break;
+		else cout << "Bad value, choose again" << endl;
+	}
+	cout << "Continue (y/n>? ";
+	cin >> answer;
+	if(answer == 'y') continue;
+	else if (answer == 'n') break;
 	else cout << "Bad value, choose again" << endl;
 	}
 	return 0;
